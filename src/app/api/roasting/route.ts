5b6a8853191231b0bb347035f869ffcfda2b9c47
Sadeli Ali <152187061+sadeli99@users.noreply.github.com>
2024-08-14 14:45:48 +0700

    Update route.ts

--- src/app/api/roasting/route.ts
+++ src/app/api/roasting/route.ts
@@ -6,97 +6,86 @@ import {
 } from "@google/generative-ai";
 import { RateLimiterMemory } from "rate-limiter-flexible";
 
+// Inisialisasi Google Generative AI dengan kunci API dari lingkungan
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
-// Konfigurasi Rate Limiter
+// Konfigurasi Rate Limiter untuk membatasi jumlah permintaan per IP
 const rateLimiter = new RateLimiterMemory({
-  points: 5, // Number of requests
-  duration: 720, // seconds
+  points: 5, // Maksimal 5 permintaan per 720 detik (12 menit)
+  duration: 720, // Durasi dalam detik untuk reset limit
 });
 
+// Fungsi untuk memformat data profil pengguna menjadi string yang mudah dibaca
 const formatData = (objs: {}) => {
-  let biodata = ""
+  let biodata = "";
   const obj = objs as { [key: string]: any };
-  Object.keys(obj).forEach(key => {
-    const title = key.replace(/_/g, ' ')
-    let value = `${obj[key]}`
-    if (title){
-      biodata += `${title[0].toUpperCase()+title.slice(1)}: ${value}`
+  
+  // Iterasi setiap kunci dalam objek profil pengguna
+  Object.keys(obj).forEach((key) => {
+    // Ubah kunci menjadi format yang lebih manusiawi (misalnya, ubah "_" menjadi spasi)
+    const title = key.replace(/_/g, " ");
+    let value = `${obj[key]}`;
+    
+    // Format judul dan tambahkan ke string biodata
+    if (title) {
+      biodata += `${title[0].toUpperCase() + title.slice(1)}: ${value}`;
     } else {
-      biodata += `${title}: ${value}`
+      biodata += `${title}: ${value}`;
     }
-    biodata += "\n"
+    
+    // Tambahkan baris baru setelah setiap pasangan kunci-nilai
+    biodata += "\n";
   });
-  return biodata
-}
+  return biodata;
+};
 
+// Fungsi utama yang menangani permintaan POST untuk melakukan roasting
 export async function POST(request: Request) {
-  // Rate Limiting
+  // Mendapatkan IP klien dari header permintaan
   const clientIp = request.headers.get("x-forwarded-for");
+  
+  // Jika IP klien ditemukan, coba konsumsi limit rate untuk IP tersebut
   if (clientIp) {
     try {
       await rateLimiter.consume(clientIp);
     } catch (error) {
+      // Jika limit tercapai, kembalikan respons 429 (Too Many Requests)
       return NextResponse.json(
         { message: "Too many requests, please try again later." },
         { status: 429 }
       );
     }
   } else {
+    // Jika IP klien tidak valid atau tidak ditemukan, kembalikan respons 400 (Bad Request)
     return NextResponse.json(
       { message: "Invalid client IP." },
       { status: 400 }
     );
   }
 
-  const { username, profile, language } = await request.json();
+  // Ambil data JSON dari permintaan, termasuk username dan profil
+  const { username, profile } = await request.json();
+  
+  // Jika username atau profil tidak disertakan, kembalikan respons 400 (Bad Request)
   if (!username || !profile) {
     return NextResponse.json(
       { message: "Username and profile data are required" },
       { status: 400 }
     );
   }
-  delete profile.avatar
+  
+  // Hapus avatar dari data profil karena tidak diperlukan
+  delete profile.profile_pic_url;
+  
+  // Format data profil menjadi string biodata yang dapat digunakan dalam prompt
   const biodata = formatData(profile);
 
   try {
-    let prompt = "";
+    // Buat prompt roasting menggunakan biodata dalam bahasa Indonesia
+    const prompt = `Berikan roasting singkat dengan kejam, menyindir, serta menyakitkan dalam bahasa gaul untuk profile Instagram berikut : ${username}. Berikut detailnya: ${biodata}`;
+    prompt += `(berikan response dalam bahasa indonesia dan jangan berikan pujian atau saran)`;
 
-    // Tentukan prompt berdasarkan bahasa yang dipilih
-    switch (language) {
-      case "indonesian":
-        prompt = `Berikan roasting singkat dengan kejam, menyindir, serta menyakitkan dalam bahasa gaul untuk profile Instagram berikut : ${username}. Berikut detailnya: ${biodata}`;
-        prompt += `(berikan response dalam bahasa indonesia dan jangan berikan pujian atau saran)`;
-        break;
-      case "english":
-        prompt = `Give a short and harsh roasting in a brutal, sarcastic, and hurtful manner for the following Instagram profile: ${username}. Here are the details: ${biodata}`;
-        prompt += `(provide the response in English and do not give any compliments or advice)`;
-        break;
-      case "mandarin":
-        prompt = `请用中文为以下Tiktok个人资料进行简短而残酷、讽刺且尖锐的吐槽：${username}。以下是详细信息：${biodata}`;
-        prompt += `(请用中文回答，不要给出任何赞美或建议)`;
-        break;
-      case "japanese":
-        prompt = `次のTiktokプロフィールに対して、残酷で皮肉で痛烈な短いローストを日本語でしてください: ${username}。詳細はこちら: ${biodata}`;
-        prompt += `(日本語で応答し、褒め言葉やアドバイスはしないでください)`;
-        break;
-      case "korean":
-        prompt = `다음 Tiktok 프로필에 대해 짧고 가혹한 한국어로 잔인하고 비꼬며 상처 주는 로스트를 해주세요: ${username}。세부 정보는 다음과 같습니다: ${biodata}`;
-        prompt += `(응답은 한국어로 제공하고 칭찬이나 조언은 하지 마세요)`;
-        break;
-      case "vietnamese":
-        prompt = `Hãy đưa ra một lời nhận xét ngắn gọn nhưng tàn nhẫn, mỉa mai và gây tổn thương bằng tiếng Việt cho hồ sơ Instagram sau: ${username}. Đây là chi tiết: ${biodata}`;
-        prompt += `(cung cấp phản hồi bằng tiếng Việt và không đưa ra bất kỳ lời khen ngợi hoặc lời khuyên nào)`;
-        break;
-      case "filipino":
-        prompt = `Magbigay ng maikli ngunit malupit, mapanuyang, at masakit na roasting sa Filipino para sa sumusunod na profile ng Instagram: ${username}. Narito ang mga detalye: ${biodata}`;
-        prompt += `(magbigay ng tugon sa Filipino at huwag magbigay ng anumang papuri o payo)`;
-        break;
-      default:
-        prompt = `Berikan roasting singkat dengan kejam, menyindir, serta menyakitkan dalam bahasa gaul untuk profile Instagram berikut : ${username}. Berikut detailnya: ${biodata}`;
-        prompt += `(berikan response dalam bahasa indonesia dan jangan berikan pujian atau saran)`;
-    }
-
+    // Konfigurasi pengaturan keamanan untuk mencegah konten yang tidak diinginkan
     const safetySettings = [
       {
         category: HarmCategory.HARM_CATEGORY_HARASSMENT,
@@ -108,15 +97,20 @@ export async function POST(request: Request) {
       },
     ];
 
+    // Dapatkan model generatif dari Google Generative AI dengan pengaturan keamanan yang ditentukan
     const model = genAI.getGenerativeModel({
       model: "gemini-1.5-flash",
       safetySettings,
     });
 
+    // Buat konten berdasarkan prompt yang telah dibuat
     const result = await model.generateContent(prompt);
     const response = await result.response;
+    
+    // Kembalikan hasil roasting dalam format JSON
     return NextResponse.json({ roasting: response.text() });
   } catch (error) {
+    // Jika terjadi kesalahan saat menghasilkan roasting, log error dan kembalikan respons 500 (Internal Server Error)
     console.error("Error generating roast:", error);
     return NextResponse.json(
       { message: "Error generating roast" },
